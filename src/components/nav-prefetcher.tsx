c69fe737e0f24f70b03cd8efa0f5f2884affc1a4
ndrepepe <apriyo@gmail.com>
2026-07-09 22:47:00 +0700

    Add navigation prefetcher

---
+++ src/components/nav-prefetcher.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export function NavPrefetcher({ hrefs }: { hrefs: string[] }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    hrefs.forEach((href) => router.prefetch(href));
+  }, [hrefs, router]);
+
+  return null;
+}
